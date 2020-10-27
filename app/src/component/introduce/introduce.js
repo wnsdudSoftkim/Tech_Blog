a6c2792285d08f758d852ebca85d13c271a6da58
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2020-10-27 22:04:32 +0900

    UI #2 Introduce

--- app/src/component/introduce/introduce.js
+++ app/src/component/introduce/introduce.js
@@ -1,1 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
+import styled from 'styled-components'
+import img from '../image/user.png'
+import IntroduceInfo from './introduceinfo'
+import {MdKeyboardReturn} from 'react-icons/md'
+import {Link} from 'react-router-dom'
+import Header from '../Header'
+
+function Introduce() {
+
+}
+export default Introduce
