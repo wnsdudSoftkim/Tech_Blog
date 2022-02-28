452f06f8ccca83e90aab8f80ccac0ee7e398c98e
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-02-28 22:43:22 +0900

    change server directory name Server -> server_v1

--- Server/Server/comment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('',views.commentPost, name='commentpost'),
-    path('fetchcomment',views.fetchComment, name="fetchcomment"),
-     path('deletecomment',views.deleteComment, name="deletecomment")
-]
--- Server/Server/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('',views.MyUser, name='main'),
-    path('login',views.login,name='login')
-]
