f3f5ab8bc8e4b27799a8086e8e346b4cd3036a35
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2022-03-21 21:20:12 +0900

    add search api setting

---
+++ server_v2/logic/search.py
@@ -0,0 +1,13 @@
+class SearchManager(object):
+
+    @classmethod
+    async def get_search(cls):
+        pass
+
+    @classmethod
+    async def search_task(cls):
+        pass
+
+    @classmethod
+    async def search_suggestion(cls):
+        pass
